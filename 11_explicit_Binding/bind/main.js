ec084d141c7037a2dd16a13362ec08eae33c0eec
Manav <manavboghraa12@gmail.com>
2025-06-24 12:21:57 +0530

    add an example in main file

--- 11_explicit_Binding/bind/main.js
+++ 11_explicit_Binding/bind/main.js
@@ -1,9 +1,31 @@
+/*****it bind and keep a copy for later*******///
 
-function bindCheck(...argu){
-    console.log(this,...argu);
+
+// function bindCheck(...argu){
+//     console.log(this,...argu);
+// }
+
+// const bindCheckOne = bindCheck.bind("Hello",1,2,3)
+// const bindCheckTwo = bindCheckOne.bind("User",5,6,7)
+
+// console.log(bindCheckTwo(7,98))
+
+let showName = {
+    firstname: "Rohit",
+    lastname: "Sharma",
 }
 
-const bindCheckOne = bindCheck.bind("Hello",1,2,3)
-const bindCheckTwo = bindCheckOne.bind("User",5,6,7)
 
-console.log(bindCheckTwo(7,98))
+let showNameOne = {
+    firstname: "MS",
+    lastname: "Dhoni",
+}
+
+let printName = function (hometown,state) {
+    console.log(`${this.firstname} ${this.lastname} from ${hometown} state ${state}`)
+}
+
+// const printOutput = printName.bind(showName,["Mumbai","MH"])  
+const printOutput = printName.bind(showName,"Mumbai","MH")
+
+printOutput()
