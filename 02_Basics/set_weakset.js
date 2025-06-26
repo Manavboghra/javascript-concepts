bd2338a7c5e1607cbe4b4add46b009442b6e5f52
Manav <manavboghraa12@gmail.com>
2025-06-26 11:41:21 +0530

    new file added

---
+++ 02_Basics/set_weakset.js
@@ -0,0 +1,48 @@
+// let arr = [1,2,3,4,5,3,2,3,4,10,9]
+// let set = new Set(arr)
+// console.log(set)
+// console.log(arr)
+
+
+// let arr1 = [1,2,3,4,5,3,2,3,4,10,9]
+// let set1 = new WeakSet(arr1)
+// console.log(set1)
+// console.log(arr1)
+
+// let set = new WeakSet()
+// set.add({userName:"Rohan", pass:"R@123"})
+// console.log(set)
+// // console.log()
+
+// let set1 = new Set()
+// set1.add({userName:"Rohan", pass:"R@123"})
+// console.log(set1)
+
+// let set2 = new Set()
+// set2.add([1,21,231,13,23,21])
+// console.log(set2)
+
+
+// let set3 = new WeakSet()
+// let arr = [1,21,231,13,23,21]
+// set3.add(arr)
+// console.log(set3)
+// console.log(set3.has([1,21,231,13,23,21])); //false
+// console.log(set3.has(arr)); //true
+
+
+// const weakset = new WeakSet()
+// weakset.add(1)
+// weakset.add(2)
+// weakset.add("Hello")
+// weakset.add(undefined)
+// console.log(weakset)
+
+// const set = new Set()
+const set = new Set()
+set.add(1)
+set.add(2)
+set.add("Hello")
+set.add(undefined)
+console.log(set)
+
