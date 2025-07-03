7d15cea371e8a01c48cd79ca50c29640b8c96d0d
Manav <manavboghraa12@gmail.com>
2025-07-03 17:34:09 +0530

    examples of iterators and generators

--- 12_iterators_generators/generators.js
+++ 12_iterators_generators/generators.js
@@ -13,3 +13,51 @@ console.log(print.next());
 console.log(print.next());
 console.log(print.next());
 console.log(print.next());
+
+// function* gen(){
+//   console.log("First!");
+//    yield 1;
+
+//    console.log("Second!");
+//    yield 3;
+
+//    console.log("Third!");
+//    yield 5;
+
+//    console.log("Done!")
+
+// }
+// const num = gen()
+// // for (const w of num) {
+// //   console.log(w)
+// // }
+// console.log(num.next())
+// console.log(num.next())
+// console.log(num.next())
+// console.log(num.next())
+
+// function* intNumber() {
+//   for (let i = 0; i < 5; i++)
+//     yield i;
+// }
+// const num = intNumber()
+
+// for (const e of num) {
+//   console.log(e)
+// }
+// console.log(num.next())
+// console.log(num.next())
+// console.log(num.next())
+// console.log(num.next())
+// console.log(num.next())
+// console.log(num.next())
+
+function* gen() {
+   yield yield  10;
+}
+
+var seq = gen();
+
+console.log(seq.next())
+console.log(seq.next(30))
+console.log(seq.next(40))
--- 12_iterators_generators/iterators.js
+++ 12_iterators_generators/iterators.js
@@ -1,28 +1,109 @@
-function fruits(value) {
-    let nextIndex = 0
+// function fruits(value) {
+//     let nextIndex = 0
 
-    return {
-        next: function () {
-            if (nextIndex < value.length)
-                return {
-                    value: value[nextIndex++],
-                    done: false,
-                }
-            else
-                return {
-                    done: true
-                }
-        }
-    }
+//     return {
+//         next: function () {
+//             if (nextIndex < value.length)
+//                 return {
+//                     value: value[nextIndex++],
+//                     done: false,
+//                 }
+//             else
+//                 return {
+//                     done: true
+//                 }
+//         }
+//     }
+// }
+
+
+// const fruit = ["Apple", "Litchi", "Kiwi", "Orange"]
+// console.log(fruit)
+
+// const fruitIndex = fruits(fruit)
+// console.log(fruitIndex.next())
+// console.log(fruitIndex.next())
+// console.log(fruitIndex.next())
+// console.log(fruitIndex.next())
+// console.log(fruitIndex.next())
+
+
+
+// function arr(value){
+
+//     let nextIndexNumber = 0
+
+//     return {
+//         next: function(){
+//             if(nextIndexNumber<value.length){
+//                 return {
+//                     value: value[nextIndexNumber++],
+//                     done: false
+//                 }
+//             }
+//             else{
+//                 return{
+//                     done:true
+//                 }
+//             }
+//         }
+//     }
+// }
+
+// const player = ["MSD","ROHIT","VIRAT","SANJU"]
+// const players = arr(player)
+// console.log(players.next())
+// console.log(players.next())
+// console.log(players.next())
+// console.log(players.next())
+// console.log(players.next())
+// console.log(players.next())
+
+
+// const values = [1,2,3,4]
+// const var1 = values[Symbol.iterator]()
+// console.log(var1)
+// console.log(var1.next())
+// console.log(var1.next())
+// console.log(var1.next())
+// console.log(var1.next())
+// console.log(var1.next())
+
+/********************************Behind the process of FOR...OF loop ********************************/
+
+// var nums = [1, 5, 16];
+
+// for (
+//    // initialisation
+//    var iter = nums[Symbol.iterator](),
+//    next = iter.next(),
+//    num = next.value;
+
+//    // iterative check
+//    !next.done;
+
+//    // iterative expressions
+//    next = iter.next(),
+//    num = next.value
+// )
+
+// {
+//    console.log(num);
+// }
+
+
+
+
+Number.prototype[Symbol.iterator]=function(){
+    return this.toString()[Symbol.iterator]()
+}
+let num = 12223
+for (const element of num) {
+    console.log(element)
 }
 
 
-const fruit = ["Apple", "Litchi", "Kiwi", "Orange"]
-console.log(fruit)
+// var arr = [3, 4, 5];
+// var arr2 = [1, 2,...arr, 6];
 
-const fruitIndex = fruits(fruit)
-console.log(fruitIndex.next())
-console.log(fruitIndex.next())
-console.log(fruitIndex.next())
-console.log(fruitIndex.next())
-console.log(fruitIndex.next())
+// console.log(arr2);
